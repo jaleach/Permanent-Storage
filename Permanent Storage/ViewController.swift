533e0e97795ee6b28f5bd229ceb507031c0e3973
jim leach <yogajim@gmail.com>
2016-10-20 11:56:58 -0400

    Initial Commit

---
+++ Permanent Storage/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Permanent Storage
+//
+//  Created by James Leach on 12/31/15.
+//  Copyright © 2015 dadio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //NSUserDefaults.standardUserDefaults().setObject("Jim", forKey: "name")
+        
+        let userName = NSUserDefaults.standardUserDefaults().objectForKey("name")! as! String
+        
+        print(userName)
+        
+        let arr = [1, 2, 3, 4]
+        
+        NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "array")
+        
+        let returnedArray = NSUserDefaults.standardUserDefaults().objectForKey("array")! as! NSArray
+        
+        for x in returnedArray{
+        
+            print(x)
+            
+        }
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
